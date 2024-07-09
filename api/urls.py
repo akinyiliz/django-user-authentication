37bb5c6b0f6e377097a6aa81982f467a66909a42
akinyiliz <lizzieke84@gmail.com>
2024-07-09 18:09:35 +0300

    feat: Add endpoints

---
+++ api/urls.py
@@ -0,0 +1,14 @@
+
+
+from django.urls import path
+from .views import CreateUser, UserLogin, GetUser, Getorganizations, AddUserToOrganization, CreateOrganization, GetOrganization
+
+urlpatterns = [
+    path('auth/register', CreateUser),
+    path('auth/login', UserLogin),
+    path('api/users/<str:pk>', GetUser),
+    path('api/organisations', Getorganizations),
+    path('api/organisations/<uuid:org_id>', GetOrganization),
+    path('api/organisations/<uuid:org_id>/users', AddUserToOrganization),
+    path('api/organisations/create', CreateOrganization),
+]
