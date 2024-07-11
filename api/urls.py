079944ee9647add6452fa6e679d236b9f23fdd5a
akinyiliz <lizzieke84@gmail.com>
2024-07-11 12:09:52 +0300

    chore: Update api/urls.py to use OrganizationView

--- api/urls.py
+++ api/urls.py
@@ -1,14 +1,14 @@
 
 
 from django.urls import path
-from .views import CreateUser, UserLogin, GetUser, Getorganizations, AddUserToOrganization, CreateOrganization, GetOrganization
+from .views import CreateUser, UserLogin, GetUser, OrganizationView, AddUserToOrganization, GetOrganization
 
 urlpatterns = [
     path('auth/register', CreateUser),
     path('auth/login', UserLogin),
     path('api/users/<str:pk>', GetUser),
-    path('api/organisations', Getorganizations),
+    path('api/organisations', OrganizationView.as_view()),
     path('api/organisations/<uuid:org_id>', GetOrganization),
     path('api/organisations/<uuid:org_id>/users', AddUserToOrganization),
-    path('api/organisations/create', CreateOrganization),
+    # path('api/organisations/create', CreateOrganization),
 ]
